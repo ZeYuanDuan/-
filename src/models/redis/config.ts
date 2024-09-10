ba7c1b574e934a1c3030fb87348a776c0edb7940
ZeYuanDuan <zyd.zeyuanduan@gmail.com>
2024-09-10 15:54:15 +0800

    feat: front-end get init data from mySQL, get current data from redis

--- src/models/redis/config.ts
+++ src/models/redis/config.ts
@@ -1,8 +1,10 @@
 import Redis from "ioredis";
+import dotenv from "dotenv";
+dotenv.config();
 
 const redisClient = new Redis({
   host: process.env.REDIS_HOST,
-  port: parseInt(process.env.REDIS_PORT || "6379"),
+  port: parseInt(process.env.REDIS_PORT || "6380"),
   password: process.env.REDIS_PASSWORD,
   maxRetriesPerRequest: null,
   enableOfflineQueue: false,

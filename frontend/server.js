065244d5f912e9bd660c0556f6d66a5de3834b15
ZeYuanDuan <zyd.zeyuanduan@gmail.com>
2024-11-10 16:04:14 +0800

    front-end: adding express router server

---
+++ frontend/server.js
@@ -0,0 +1,52 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 5500;
+
+// 設置靜態文件目錄
+app.use(express.static(__dirname));
+
+// 路由設置
+const router = express.Router();
+
+// 主頁重定向
+router.get("/", (req, res) => {
+  res.redirect("/home");
+});
+
+// 各個頁面路由
+router.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "home/home.html"));
+});
+
+router.get("/voting", (req, res) => {
+  res.sendFile(path.join(__dirname, "voting/voting.html"));
+});
+
+router.get("/participant-vote", (req, res) => {
+  res.sendFile(path.join(__dirname, "participant-vote/voting.html"));
+});
+
+router.get("/create-vote", (req, res) => {
+  res.sendFile(path.join(__dirname, "create-vote/create-vote.html"));
+});
+
+router.get("/update-vote", (req, res) => {
+  res.sendFile(path.join(__dirname, "update-vote/update-vote.html"));
+});
+
+// 404 處理
+router.use((req, res) => {
+  res.status(404).send("頁面不存在");
+});
+
+app.use("/", router);
+
+app.listen(PORT, () => {
+  console.log(`服務器運行在 http://localhost:${PORT}`);
+});

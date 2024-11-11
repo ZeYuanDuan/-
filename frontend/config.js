2facdb82ae30199e5a70abe83c0d7d4c3a700eb0
ZeYuanDuan <zyd.zeyuanduan@gmail.com>
2024-11-11 14:29:41 +0800

    feat: finish multi-container architecture with Express frontend, Node.js backend, RabbitMQ, and Redis services

--- frontend/config.js
+++ frontend/config.js
@@ -1,10 +1,10 @@
+// ! 容器間通訊用的配置
 export const API_BASE_URL = "http://localhost:3000";
-export const FRONTEND_BASE_URL = "http://127.0.0.1:5500";
+export const FRONTEND_BASE_URL = "http://localhost:3301";
 export const WEBSOCKET_URL = "http://localhost:3000";
-export const DATE_TIME_FORMAT = "zh-TW"; // 時間顯示格式：YYYY/MM/DD HH:mm:ss
-export const TIME_ZONE = "Asia/Taipei"; // 時區
+export const DATE_TIME_FORMAT = "zh-TW";
+export const TIME_ZONE = "Asia/Taipei";
 
-// 頁面路由配置
 export const ROUTES = {
   HOME: "/home/home.html",
   VOTING: "/voting/voting.html",
@@ -13,7 +13,23 @@ export const ROUTES = {
   UPDATE_VOTE: "/update-vote/update-vote.html",
 };
 
-// 原本寫在以下路徑的配置： frontend\home\scripts\config.js
+// ! 本地測試用的配置
+// export const API_BASE_URL = "http://localhost:3000";
+// export const FRONTEND_BASE_URL = "http://127.0.0.1:5500";
+// export const WEBSOCKET_URL = "http://localhost:3000";
+// export const DATE_TIME_FORMAT = "zh-TW"; // 時間顯示格式：YYYY/MM/DD HH:mm:ss
+// export const TIME_ZONE = "Asia/Taipei"; // 時區
+
+// // 頁面路由配置
+// export const ROUTES = {
+//   HOME: "/home/home.html",
+//   VOTING: "/voting/voting.html",
+//   PARTICIPANT_VOTE: "/participant-vote/voting.html",
+//   CREATE_VOTE: "/create-vote/create-vote.html",
+//   UPDATE_VOTE: "/update-vote/update-vote.html",
+// };
+
+// ! 原本寫在以下路徑的配置： frontend\home\scripts\config.js
 // export const API_BASE_URL = "http://localhost:3000";
 // export const BASE_URL = "http://127.0.0.1:5500/public";
 // export const DATE_TIME_FORMAT = "zh-TW"; // 時間顯示格式：YYYY/MM/DD HH:mm:ss
